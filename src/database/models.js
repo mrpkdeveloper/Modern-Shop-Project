33be57f82c77615a94c337c507dc2a3769b6ccc4
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-07-14 13:43:29 +0530

    database created successfully

---
+++ src/database/models.js
@@ -0,0 +1,44 @@
+const { db } = require('./connection')
+const { DataTypes } = require('sequelize')
+
+const users = db.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+})
+
+
+const products = db.define('products', {              //table name products with following attributes
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: DataTypes.STRING,
+    manufacturer: DataTypes.STRING,
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: true,
+        defaultValue: 0.0
+
+    },
+    productimage: {
+        type: DataTypes.STRING
+    }
+
+})
+
+//test
+db.sync()
+    .then(() => console.log('database synced successfully'))
+    .catch((err) => console.error("error in creating database"))
+
+module.exports = {
+    db, users, products
+}
