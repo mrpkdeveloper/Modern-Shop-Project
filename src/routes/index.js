37206a0c1672498d388bebaaa634d17ff379c84f
mrpkdeveloper <mrpkdeveloper7@gmail.com>
2020-07-16 18:33:02 +0530

    errors in merging

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const route = require('express').Router()
 
-// route.use('/users', require('./users'))
 route.use('/products', require('./product'))
 route.use('/cart', require('./cart'))
 exports = module.exports = {
